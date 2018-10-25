4d5d3b5fe78fed17ff78fb3b8dac972e1b7ecbd0
Patrick McCann <pgm5@st-andrews.ac.uk>
2018-10-25 11:30:36 +0100

    Removing consent screen

--- catch/src/main/java/uk/ac/masts/sifids/activities/SettingsActivity.java
+++ catch/src/main/java/uk/ac/masts/sifids/activities/SettingsActivity.java
@@ -7,38 +7,22 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.GsonBuilder;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 
 import uk.ac.masts.sifids.R;
-import uk.ac.masts.sifids.singletons.RequestQueueSingleton;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -165,8 +149,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 || PortDetailsPreferenceFragment.class.getName().equals(fragmentName)
                 || GearDetailsPreferenceFragment.class.getName().equals(fragmentName)
                 || SpeciesDetailsPreferenceFragment.class.getName().equals(fragmentName)
-                || BuyerDetailsPreferenceFragment.class.getName().equals(fragmentName)
-                || ConsentDetailsPreferenceFragment.class.getName().equals(fragmentName);
+                || BuyerDetailsPreferenceFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -344,90 +327,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
-    /**
-     * This fragment shows general preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class ConsentDetailsPreferenceFragment extends BasePreferenceFragment {
-
-        SharedPreferences prefs;
-        SharedPreferences.Editor editor;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_consent_details);
-            prefs = PreferenceManager.getDefaultSharedPreferences(
-                    getActivity().getApplicationContext());
-            editor = prefs.edit();
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.consent_name_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.consent_email_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.consent_phone_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_vessel_pln_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_vessel_name_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_owner_master_name_key)));
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.consent_accept_all_key)));
-
-            Preference allTheAbove = (Preference) findPreference(getString(R.string.consent_accept_all_key));
-            allTheAbove.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    Boolean accepted = (Boolean) newValue;
-                    SharedPreferences.Editor editor = prefs.edit();
-                    String[] consentPrefKeys = {
-                            getString(R.string.consent_read_understand_key),
-                            getString(R.string.consent_questions_opportunity_key),
-                            getString(R.string.consent_questions_answered_key),
-                            getString(R.string.consent_can_withdraw_key),
-                            getString(R.string.consent_confidential_key),
-                            getString(R.string.consent_data_archiving_key),
-                            getString(R.string.consent_risks_key),
-                            getString(R.string.consent_take_part_key),
-                            getString(R.string.consent_photography_capture_key),
-                            getString(R.string.consent_photography_publication_key),
-                            getString(R.string.consent_photography_future_studies_key),
-                            getString(R.string.consent_fish_1_key)
-                    };
-                    for (String prefKey : consentPrefKeys) {
-                        editor.putBoolean(prefKey, accepted);
-                        editor.apply();
-                    }
-                    return true;
-                }
-            });
-
-            Preference consentName = (Preference) findPreference(getString(R.string.consent_name_key));
-            consentName.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    String name = (String) newValue;
-                    if (!name.isEmpty()) {
-                        String ownerMasterName =
-                                prefs.getString(
-                                        getString(R.string.pref_owner_master_name_key), "");
-                        if (ownerMasterName.isEmpty()) {
-                            EditTextPreference ownerMasterNamePref =
-                                    (EditTextPreference) findPreference(
-                                            getString(R.string.pref_owner_master_name_key));
-                            ownerMasterNamePref.setText(name);
-                            ownerMasterNamePref.setSummary(name);
-
-                        }
-                    }
-                    EditTextPreference namePref = (EditTextPreference) preference;
-                    namePref.setSummary(name);
-                    return true;
-                }
-            });
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -467,81 +366,4 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    public void submitConsentDetails(View view) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
-                getBaseContext().getApplicationContext());
-        final Map<String, ?> prefsMap = prefs.getAll();
-        String[] consentPrefKeys = {
-                getString(R.string.consent_read_understand_key),
-                getString(R.string.consent_questions_opportunity_key),
-                getString(R.string.consent_questions_answered_key),
-                getString(R.string.consent_can_withdraw_key),
-                getString(R.string.consent_confidential_key),
-                getString(R.string.consent_data_archiving_key),
-                getString(R.string.consent_risks_key),
-                getString(R.string.consent_take_part_key),
-                getString(R.string.consent_photography_capture_key),
-                getString(R.string.consent_photography_publication_key),
-                getString(R.string.consent_photography_future_studies_key),
-                getString(R.string.consent_name_key),
-                getString(R.string.consent_email_key),
-                getString(R.string.consent_phone_key),
-                getString(R.string.consent_fish_1_key),
-                getString(R.string.consent_name_key),
-                getString(R.string.pref_vessel_pln_key),
-                getString(R.string.pref_vessel_name_key),
-                getString(R.string.pref_owner_master_name_key),
-        };
-        JSONObject consentJson = new JSONObject();
-        boolean goodToGo = true;
-        for (String prefKey : consentPrefKeys) {
-            Object pref = prefsMap.get(prefKey);
-            if (pref == null
-                    || (pref instanceof Boolean && !((boolean) pref))
-                    || (pref instanceof String && ((String) pref).isEmpty())) {
-                goodToGo = false;
-                Toast.makeText(getBaseContext(), getString(R.string.participant_consent_incomplete),
-                        Toast.LENGTH_LONG).show();
-                break;
-            }
-            try {
-                consentJson.put(prefKey, pref.toString());
-            } catch (JSONException jse) {
-                goodToGo = false;
-                break;
-            }
-        }
-        if (goodToGo) {
-            final String url = getBaseContext().getString(R.string.post_request_url);
-            final SharedPreferences.Editor editor = prefs.edit();
-            JsonObjectRequest request = new JsonObjectRequest(
-                    Request.Method.POST, url, consentJson,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject jsonObject) {
-                            editor.putBoolean(getString(R.string.consent_confirmed_key), true);
-                            editor.apply();
-                            Toast.makeText(getBaseContext(), getString(R.string.participant_consent_thank_you),
-                                    Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(SettingsActivity.this, SettingsActivity.class);
-                            startActivity(intent);
-                            SettingsActivity.this.finish();
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            editor.putBoolean(getString(R.string.consent_confirmed_key), false);
-                            editor.apply();
-                            Toast.makeText(getBaseContext(), getString(R.string.participant_consent_error),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    }
-            );
-            RequestQueueSingleton.getInstance(getBaseContext()).addToRequestQueue(request);
-
-        }
-    }
-
 }
