868ab7b9a3cced8bf1e4535c9fb4720d567923c0
Patrick McCann <pgm5@st-andrews.ac.uk>
2018-10-26 10:27:02 +0100

    Removing tests related to ICES Squares

--- catch/src/test/java/uk/ac/masts/sifids/LocationUnitTest.java
+++ catch/src/test/java/uk/ac/masts/sifids/LocationUnitTest.java
@@ -16,38 +16,6 @@ import static org.junit.Assert.*;
 public class LocationUnitTest {
 
     @Test
-    public void icesSquares_CalculatedCorrectly() throws Exception {
-        assertEquals(null, CatchLocation.getIcesRectangle(35.9, 0));
-        assertEquals(null, CatchLocation.getIcesRectangle(85.6, 0));
-        assertEquals(null, CatchLocation.getIcesRectangle(50, -44.1));
-        assertEquals(null, CatchLocation.getIcesRectangle(50, 68.6));
-        assertEquals("37F3", CatchLocation.getIcesRectangle(54.1, 3.1));
-        assertEquals("45G0", CatchLocation.getIcesRectangle(58.2, 10.51));
-        assertEquals("45A0", CatchLocation.getIcesRectangle(58.2, -43.51));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.001));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.1));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.2));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.3));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.4));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.5));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.6));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.7));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.8));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.9));
-        assertEquals("49E9", CatchLocation.getIcesRectangle(60.2, -0.999));
-        assertNotEquals("41E11", CatchLocation.getIcesRectangle(56.333, -2.8));
-    }
-
-    @Test
-    public void icesSquareBounds_CalculatedCorrectly() throws Exception {
-        Map bounds = CatchLocation.getIcesRectangleBounds(60.2, -0.5);
-        assertEquals(60.0, bounds.get(CatchLocation.LOWER_LAT));
-        assertEquals(60.5, bounds.get(CatchLocation.UPPER_LAT));
-        assertEquals(-1.0, bounds.get(CatchLocation.LOWER_LONG));
-        assertEquals(-0.0, bounds.get(CatchLocation.UPPER_LONG));
-    }
-
-    @Test
     public void latitudeComponents_CalculatedCorrectly() throws Exception {
         CatchLocation loc = new CatchLocation();
         loc.setLatitude(12.4);
